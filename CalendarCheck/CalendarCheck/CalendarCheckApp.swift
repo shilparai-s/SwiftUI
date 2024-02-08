d078d41c6ab91afc328fa51d2d20933dda286c4b
shilparai-s <shilparai.s@gmail.com>
2024-02-08 20:35:59 +0530

    Add files via upload

---
+++ CalendarCheck/CalendarCheck/CalendarCheckApp.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarCheckApp.swift
+//  CalendarCheck
+//
+//  Created by Shilpa Seetharam on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
